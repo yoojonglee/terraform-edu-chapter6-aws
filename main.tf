cd9eb274244c3b6abf5f8c968cef079ae8a6c0b2
Gyuseok Lee <hahohh@gmail.com>
2022-07-13 10:46:51 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  // cloud {
+  //   organization = "<MY_ORG_NAME>"         # 생성한 ORG 이름 지정
+  //   hostname     = "app.terraform.io"      # default
+
+  //   workspaces {
+  //     name = "terraform-edu-chapter6-aws"  # 없으면 생성됨
+  //   }
+  // }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -137,24 +145,12 @@ resource "aws_instance" "hashicat" {
   }
 }
 
-# We're using a little trick here so we can run the provisioner without
-# destroying the VM. Do not do this in production.
-
-# If you need ongoing management (Day N) of your virtual machines a tool such
-# as Chef or Puppet is a better choice. These tools track the state of
-# individual files and can keep them in the correct configuration.
-
-# Here we do the following steps:
-# Sync everything in files/ to the remote VM.
-# Set up some environment variables for our script.
-# Add execute permissions to our scripts.
-# Run the deploy_app.sh script.
 resource "null_resource" "configure-cat-app" {
   depends_on = [aws_eip_association.hashicat]
 
-  triggers = {
-    build_number = timestamp()
-  }
+  // triggers = {
+  //   build_number = timestamp()
+  // }
 
   provisioner "file" {
     source      = "files/"
