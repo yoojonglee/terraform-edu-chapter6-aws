7c4828612955e72e6c8a1e030eeaf90f52ce7f20
Great-Stone <hahohh@gmail.com>
2022-09-05 21:08:39 +0900

    output expresion change

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "catapp_url" {
   value = {
-    for instance in aws_instance.hashicat:
+    for instance in aws_instance.hashicat[*]:
       instance.id => "http://${instance.public_dns}"
   }
 }
 
 output "catapp_ip" {
   value = {
-    for instance in aws_instance.hashicat:
+    for instance in aws_instance.hashicat[*]:
       instance.id => "http://${instance.public_ip}"
   }
 }
