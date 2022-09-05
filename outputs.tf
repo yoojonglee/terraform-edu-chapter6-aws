c18dfea00999a5592425c1a572b293f71025bae0
Gyuseok Lee <hahohh@gmail.com>
2022-09-05 21:27:33 +0900

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,7 @@
 output "catapp_url" {
-  value = {
-    for instance in aws_instance.hashicat[*]:
-      instance.id => "http://${instance.public_dns}"
-  }
+  value = "http://${aws_eip.hashicat.public_dns}"
 }
 
 output "catapp_ip" {
-  value = {
-    for instance in aws_instance.hashicat[*]:
-      instance.id => "http://${instance.public_ip}"
-  }
+  value = "http://${aws_eip.hashicat.public_ip}"
 }
