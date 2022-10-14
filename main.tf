0587fd4a70a022d1dad30cefc3ad9ffdc45d066b
“yoojonglee” <yoojonglee@lguplus.co.kr>
2022-10-14 13:41:31 +0900

    Updage main.tf default tags

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
   region = var.region
   default_tags {
     tags = {
-      Project = "Coffee-Mug-Cake"
+      Project = "TOM-Coffee-Mug-Cake"
       Owner   = "jerry & tom"
     }
   }
